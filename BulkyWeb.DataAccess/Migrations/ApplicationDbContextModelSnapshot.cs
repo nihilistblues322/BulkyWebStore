863c4c1e26da32314c66fa193682b4a6b44b5b7c
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-08-07 19:01:49 +0500

    generic irepository

--- BulkyWeb.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ BulkyWeb.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -34,7 +34,8 @@ namespace BulkyWeb.DataAccess.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(30)
+                        .HasColumnType("nvarchar(30)");
 
                     b.HasKey("Id");
 
