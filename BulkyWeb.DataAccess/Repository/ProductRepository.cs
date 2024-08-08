c008fc25b6ccab0eec0e134661deeb78a488ceb8
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-08-08 15:08:53 +0500

    product db, migrations, irepo, unit of work

---
+++ BulkyWeb.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BulkyWeb.DataAccess.Data;
+using BulkyWeb.DataAccess.Repository.IRepository;
+using BulkyWeb.Models;
+
+namespace BulkyWeb.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db)
+            : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
